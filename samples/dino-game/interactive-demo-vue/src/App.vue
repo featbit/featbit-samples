4e438e9cf535ea408d7f9b1ac1d7ac4482ce46ab
Comiscience <hu-beau@outlook.com>
2022-09-29 12:33:02 +0800

    firstupload

---
+++ samples/dino-game/interactive-demo-vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import DinoGame from './components/DinoGame/DinoGame.vue'
+</script>
+
+<template>
+
+  <HelloWorld msg="Vite + Vue" />
+  <DinoGame />
+</template>
+
+<style scoped>
+
+</style>

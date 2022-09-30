85c43c645d7e5dd820708c3019849f626f7b6615
Comiscience <hu-beau@outlook.com>
2022-09-30 20:11:55 +0800

    updated

--- samples/dino-game/interactive-demo-vue/src/components/Steps/Steps.vue
+++ samples/dino-game/interactive-demo-vue/src/components/Steps/Steps.vue
@@ -38,12 +38,5 @@ export default {
             <StepGameDifficulty v-if="store.currentTaskIndex == 1" />
             <StepComplete v-if="store.currentTaskIndex == 2" />
         </div>
-        <!-- <div class="steps-action">
-            <a-button v-if="store.showNextButton == true" 
-                      style="margin-right;: 8px"
-                      type="primary" @click="store.nextStep">Next Step</a-button>
-            <a-button v-if="store.showPreviousButton == true" @click="store.previousStep()">Previous
-            </a-button>
-        </div> -->
     </div>
 </template>

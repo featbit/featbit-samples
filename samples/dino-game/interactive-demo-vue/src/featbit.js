d1f0231424ac76a88a0ac19cba0723fce75794a8
Comiscience <hu-beau@outlook.com>
2022-10-01 14:40:31 +0800

    updated

--- samples/dino-game/interactive-demo-vue/src/featbit.js
+++ samples/dino-game/interactive-demo-vue/src/featbit.js
@@ -6,44 +6,39 @@ export const flagsDefaultValues = {
     "difficulty-mode": "easy"
 }
 
+// This helps VUE project to do feature flag usage statistics, this also helps implement an easy way of bootstrap
+export const createFlagsProxy = () => {
+    return new Proxy({}, {
+        get(target, prop, receiver) {
+            return (typeof prop === 'string' && !prop.startsWith('__v_')) ?
+                ffcClient.variation(prop, flagsDefaultValues[prop] || '') : '';
+        }
+    })
+}
+
 export const featBit = {
     install(app, options) {
-        const _option = Object.assign(
-            {
-                secret: "ZTczLTFiMTctNCUyMDIyMDkyOTA1MDUwOV9fMTU5X18yMzVfXzQ1MV9fZGVmYXVsdF9lY2RjMA==",
-                user: {
-                    id: 'my-user',
-                    userName: 'my user',
-                    email: '',
-                    customizedProperties: [
-                        {
-                            "name": "sex",
-                            "value": "male"
-                        }]
-                },
-                bootstrap: Object.keys(flagsDefaultValues).map(obj => ({
-                    id: obj,
-                    variation: flagsDefaultValues[obj],
-                    variationOptions: []
-                }))
-            }
-        );
-        ffcClient.init(_option);
+        ffcClient.init({
+            secret: "ZTczLTFiMTctNCUyMDIyMDkyOTA1MDUwOV9fMTU5X18yMzVfXzQ1MV9fZGVmYXVsdF9lY2RjMA==",
+            user: {
+                id: 'my-user',
+                userName: 'my user',
+                customizedProperties: [
+                    {
+                        "name": "sex",
+                        "value": "male"
+                    }]
+            },
+        });
+        
         const store = useFeatBitStore()
-        ffcClient.on("ff_update", (changes) => changes.length ? store.setFlags(changes) : null);
-        ffcClient.waitUntilReady().then((changes) => changes.length ? store.setFlags(changes) : null);
+        ffcClient.on("ff_update", (changes) => changes.length ? store.flags = createFlagsProxy() : null);
+        ffcClient.waitUntilReady().then((changes) => changes.length ? store.flags = createFlagsProxy() : null);
     }
 }
 
 export const useFeatBitStore = defineStore('featbit', {
     state: () => ({
-        flags: flagsDefaultValues
-    }),
-    actions: {
-        setFlags(changes) {
-            changes.map(obj => {
-                this.flags[obj.id] = ffcClient.variation(obj.id, flagsDefaultValues[obj.id] || '');
-            });
-        },
-    },
+        flags: createFlagsProxy() // with proxy method, states won't be observable in VUE devtool, you can switch to "flags: flagsDefaultValues", but it's not friendly for feature flag usage statistics. You can check localstorage to know the current value of feature flag.
+    })
 })

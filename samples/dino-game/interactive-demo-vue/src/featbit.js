cc98d08302d82849cc5d0f34fab7a05ea7dbe1b7
Comiscience <hu-beau@outlook.com>
2023-11-09 13:10:19 +0800

    restore origin vue project sample code

--- samples/dino-game/interactive-demo-vue/src/featbit.js
+++ samples/dino-game/interactive-demo-vue/src/featbit.js
@@ -3,33 +3,37 @@ import { defineStore } from 'pinia';
 import { datadogRum } from '@datadog/browser-rum';
 
 export const flagsDefaultValues = {
-    // "game-runner": "false",
-    // "difficulty-mode": "easy"
+    "game-runner": "false",
+    "difficulty-mode": "easy"
 }
 
 // This helps VUE project to do feature flag usage statistics, this also helps implement an easy way of bootstrap
 export const createFlagsProxy = () => {
     return new Proxy({}, {
         get(target, prop, receiver) {
-            var returnValue =
-                (typeof prop === 'string' && !prop.startsWith('__v_')) ?
-                    fbClient.variation(prop, flagsDefaultValues[prop] || '') : '';
-            if(typeof prop === 'string'){
-                console.log(`featbit: ${typeof prop === 'string' ? prop : ''} = ${returnValue}`)
-                datadogRum.addFeatureFlagEvaluation(prop, returnValue);
-            }
-            return returnValue;
+            //
+            // Code sample for DataDog rum integration
+            //
+            // var returnValue =
+            //     (typeof prop === 'string' && !prop.startsWith('__v_')) ?
+            //         fbClient.variation(prop, flagsDefaultValues[prop] || '') : '';
+            // if(typeof prop === 'string'){
+            //     console.log(`featbit: ${typeof prop === 'string' ? prop : ''} = ${returnValue}`)
+            //     datadogRum.addFeatureFlagEvaluation(prop, returnValue);
+            // }
+            // return returnValue;
+
+            return (typeof prop === 'string' && !prop.startsWith('__v_')) ?
+                fbClient.variation(prop, flagsDefaultValues[prop] || '') : '';
         }
     })
 }
 
 export const featBit = {
     install(app, options) {
-        // const urlParams = new URLSearchParams(window.location.search);
-        // let envkey = urlParams.get("envKey");
-        // let evaluationUrl = urlParams.get("evaluationUrl");        
-        let envkey = 'gvkuIffZRkWoXWM-VumvsAJGumzSi8qUeo7MWeDXG0jQ';
-        let evaluationUrl = 'http://localhost:5100';
+        const urlParams = new URLSearchParams(window.location.search);
+        let envkey = urlParams.get("envKey");
+        let evaluationUrl = urlParams.get("evaluationUrl");        
         console.log(envkey)
         console.log(evaluationUrl)
 
@@ -66,28 +70,28 @@ export const featBit = {
 
         fbClient.waitUntilReady().then((changes) => changes.length ? store.flags = createFlagsProxy() : null);
 
-        datadogRum.setUser({
-            id: userId,
-            name: userName
-        })
-        datadogRum.init({
-            applicationId: 'ef15baa3-3d4c-4c9f-843e-4ad92bcd6b4e',
-            clientToken: 'pubb317bf6eb6d87f8cbab78c9299391288',
-            site: 'datadoghq.eu',
-            service:'featbit-feature-flag-rum',
-            env:'demo',
-            // Specify a version number to identify the deployed version of your application in Datadog 
-            // version: '1.0.0', 
-            sessionSampleRate:100,
-            sessionReplaySampleRate: 20,
-            trackUserInteractions: true,
-            trackResources: true,
-            trackLongTasks: true,
-            defaultPrivacyLevel:'mask-user-input',
-            enableExperimentalFeatures: ["feature_flags"],
-        });
-            
-        datadogRum.startSessionReplayRecording();
+        //
+        // datadog rum sample code
+        //
+        // datadogRum.setUser({
+        //     id: userId,
+        //     name: userName
+        // })
+        // datadogRum.init({
+        //     applicationId: 'ef15baa3-3d4c-4c9f-843e-4ad92bcd6b4e',
+        //     clientToken: 'pubb317bf6eb6d87f8cbab78c9299391288',
+        //     site: 'datadoghq.eu',
+        //     service:'featbit-feature-flag-rum',
+        //     env:'demo',
+        //     sessionSampleRate:100,
+        //     sessionReplaySampleRate: 20,
+        //     trackUserInteractions: true,
+        //     trackResources: true,
+        //     trackLongTasks: true,
+        //     defaultPrivacyLevel:'mask-user-input',
+        //     enableExperimentalFeatures: ["feature_flags"],
+        // });
+        // datadogRum.startSessionReplayRecording();
     }
 }
 

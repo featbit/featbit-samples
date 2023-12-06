801eeb34de6271dff0f7b76a2ccc87d6a7fbb8d1
Comiscience <hu-beau@outlook.com>
2023-12-06 21:11:26 +0800

    working in progress

---
+++ samples/dotnet/Blogs/BlazorApp-empty1/Client/Program.cs
@@ -0,0 +1,14 @@
+using BlazorApp_empty1.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorApp_empty1.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorApp_empty1.ServerAPI"));
+
+await builder.Build().RunAsync();
---
+++ samples/dotnet/Blogs/BlazorAppDemo/BlazorAppDemo/FeatureFlags/FeatureFlagsAttribute.cs
@@ -0,0 +1,45 @@
+﻿using FeatBit.Sdk.Server;
+using FeatBit.Sdk.Server.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace BlazorAppDemo.FeatureFlags
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.All, AllowMultiple = true)]
+    public class FeatureFlagsAttribute : ActionFilterAttribute, IAsyncPageFilter
+    {
+        public string[] Keys;
+        public FeatureFlagsAttribute(params string[] keys)
+        {
+            Keys = keys;
+        }
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            IFbClient fm = context.HttpContext.RequestServices.GetRequiredService<IFbClient>();
+            var user = context.HttpContext.User.Identity?.Name;
+            var fbUser = FbUser.Builder(user ??= Guid.NewGuid().ToString()).Build();
+            var enabled = fm.BoolVariation("weather-page", fbUser, false);
+            if (enabled)
+            {
+                await next.Invoke().ConfigureAwait(true);
+            }
+            else
+            {
+                context.Result = new NotFoundResult();
+            }
+        }
+
+        public Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context, PageHandlerExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

579ff2e15e4134dcee5ef3faf88a161f1f8ce572
Comiscience <hu-beau@outlook.com>
2023-12-19 18:31:48 +0800

    working in progress

---
+++ samples/integration/FeatBit.DataExport.CHToSegment/SegmentLogger.cs
@@ -0,0 +1,28 @@
+﻿using Segment.Analytics.Utilities;
+
+namespace FeatBit.DataExport.CHToSegment
+{
+    public class SegmentLogger : ISegmentLogger
+    {
+        public void Log(LogLevel logLevel, Exception exception = null, string message = null)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Warning:
+                case LogLevel.Information:
+                case LogLevel.Debug:
+                    Console.Out.WriteLine("Message: " + message);
+                    break;
+                case LogLevel.Critical:
+                case LogLevel.Trace:
+                case LogLevel.Error:
+                    Console.Error.WriteLine("Exception: " + exception?.StackTrace);
+                    Console.Error.WriteLine("Message: " + message);
+                    break;
+                case LogLevel.None:
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ samples/integration/FeatBit.DataExport.CHToSegment/SegmentProxyHttpClient.cs
@@ -0,0 +1,31 @@
+﻿using Segment.Analytics.Utilities;
+
+namespace FeatBit.DataExport.CHToSegment
+{
+    public class ProxyHttpClient : DefaultHTTPClient
+    {
+        public ProxyHttpClient(string apiKey, string apiHost = null, string cdnHost = null) : base(apiKey, apiHost, cdnHost)
+        {
+        }
+
+        public override string SegmentURL(string host, string path)
+        {
+            if (host.Equals(_apiHost))
+            {
+                return "Your proxy api url";
+            }
+            else
+            {
+                return "Your proxy cdn url";
+            }
+        }
+    }
+
+    public class ProxyHttpClientProvider : IHTTPClientProvider
+    {
+        public HTTPClient CreateHTTPClient(string apiKey, string apiHost = null, string cdnHost = null)
+        {
+            return new ProxyHttpClient(apiKey, apiHost, cdnHost);
+        }
+    }
+}
